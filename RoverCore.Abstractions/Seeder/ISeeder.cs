2b080d7b6cb503dd8eaacb9105c501b88ce778dc
Michael Tanczos <mike@logic-gate.com>
2022-03-02 18:10:03 -0500

    Added priority to seeder

--- RoverCore.Abstractions/Seeder/ISeeder.cs
+++ RoverCore.Abstractions/Seeder/ISeeder.cs
@@ -4,6 +4,15 @@ namespace RoverCore.Abstractions.Seeder;
 
 public interface ISeeder : IScoped
 {
+    /// <summary>
+    /// Priority for determining seeding order (higher numbers have first priority to seed)
+    /// </summary>
+    int Priority { get; }
+
+    /// <summary>
+    /// Method that performs the seeding
+    /// </summary>
+    /// <returns></returns>
     Task SeedAsync();
 }
 
