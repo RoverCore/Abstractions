f9a430fde00bce9ee1a208c90dfe074d4e210009
Michael Tanczos <tanczosm@eastonsd.org>
2022-03-10 12:17:56 -0500

    Added Pre Header to template

--- RoverCore.Abstractions/Templates/ITemplate.cs
+++ RoverCore.Abstractions/Templates/ITemplate.cs
@@ -6,6 +6,7 @@ public interface ITemplate
     string Slug { get; set; }
     string Name { get; set; }
     string Description { get; set; }
+    string PreHeader { get; set; }
     string Body { get; set; }
     DateTime Created { get; set; }
     DateTime Updated { get; set; }
